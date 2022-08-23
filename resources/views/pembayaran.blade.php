76b3c13f1f3862285bc5b159910d7ae14a8b63f0
Awan A <aoneholics@gmail.com>
2022-08-24 03:35:01 +0700

    production

--- resources/views/pembayaran.blade.php
+++ resources/views/pembayaran.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- @TODO: replace SET_YOUR_CLIENT_KEY_HERE with your client key -->
     <script type="text/javascript"
-      src="https://app.sandbox.midtrans.com/snap/snap.js"
-      data-client-key="SB-Mid-client-Ywx-f6GRlmJSKT_I"></script>
+      src="https://app.midtrans.com/snap/snap.js"
+      data-client-key="Mid-client-MePyTzVX9H4Pjg16"></script>
     <!-- Note: replace with src="https://app.midtrans.com/snap/snap.js" for Production environment -->
   </head>
  
